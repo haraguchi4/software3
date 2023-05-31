5837efc70215253ad08f88b45fd06b8795b9fb4e
haraguchi4 <134473416+haraguchi4@users.noreply.github.com>
2023-05-31 16:35:46 +0900

    Add files via upload

--- ソフトウェア演習3/src/absdiff2.cpp
+++ ソフトウェア演習3/src/absdiff2.cpp
@@ -1,21 +1,25 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-void absdiff(const cv::Mat &src, const cv::Mat &dst)
+int main(int argc, char **argv)
 {
+  // 元の画像と間違いが含まれた画像の読み込み
+  cv::Mat image1 = cv::imread(argv[1]);
+  cv::Mat image2 = cv::imread(argv[2]);
+
   // 読み込みの確認
-  if (src.empty() || dst.empty())
+  if (image1.empty() || image2.empty())
   {
     std::cout << "読み込みに失敗しました" << std::endl;
-    return;
+    return -1;
   }
 
   // 画像のリサイズ
-  cv::resize(dst, dst, src.size());
+  cv::resize(image2, image2, image1.size());
 
   // 画像の差分を計算
   cv::Mat diffImage;
-  cv::absdiff(src, dst, diffImage);
+  cv::absdiff(image1, image2, diffImage);
 
   // 差分画像をグレースケールに変換
   cv::cvtColor(diffImage, diffImage, cv::COLOR_BGR2GRAY);
@@ -24,7 +28,7 @@ void absdiff(const cv::Mat &src, const cv::Mat &dst)
   cv::threshold(diffImage, diffImage, 30, 255, cv::THRESH_BINARY);
 
   // 差分のある画素に赤色を付ける
-  cv::Mat resultImage = src.clone();
+  cv::Mat resultImage = image1.clone();
   resultImage.setTo(cv::Scalar(0, 0, 255), diffImage);
 
   cv::Scalar lowerRed = cv::Scalar(0, 0, 255);
@@ -41,9 +45,15 @@ void absdiff(const cv::Mat &src, const cv::Mat &dst)
   double alpha = 0.4; // 0.0から1.0の範囲で設定（1.0で元の明るさ、0.0で完全に暗くなる）
 
   // 画像を暗くする
-  cv::Mat darkenedImage = src * alpha;
+  cv::Mat darkenedImage = image1 * alpha;
 
   // redMaskと暗くした画像を重ねる
   cv::Mat overlaidImage;
   cv::addWeighted(redImage, 1.0, darkenedImage, 1.0, 0.0, overlaidImage);
+
+  // 結果を表示
+  cv::imshow("Red Extraction", overlaidImage);
+  cv::waitKey(0);
+
+  return 0;
 }
